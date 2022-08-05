b328294b86bf76b9fe77bd94c63632a3283c0f03
jckpt <krawcu333@gmail.com>
2022-08-05 12:23:21 +0200

    looping through all items in the directory

--- genFile.py
+++ genFile.py
@@ -2,39 +2,53 @@ from lib2to3.pgen2.literals import simple_escapes
 import json
 from bs4 import BeautifulSoup
 import requests
-item = "Abyssal_Scepter_item_HD.webp"
-print(item[:item.find("_item")])
-url = "https://leagueoflegends.fandom.com/wiki/Guardian_Angel"
-result = requests.get(url)
-doc = BeautifulSoup(result.text, 'html.parser')
-passiveChild = doc.find_all(text="Passive")
-statsChild = doc.find_all(text="Stats")
-consumeChild = doc.find_all(text="Consume")
-try:
-    passive = passiveChild[0].parent.parent.div.div.get_text()
-except:
-    passive = "undefined"
-print(passive)
-try:
-    stats = statsChild[0].parent.parent.find_all("div",{"data-source":True})
-    for i, stat in enumerate(stats):
-        stats[i] = stat.text.replace('\n',"")
-except:
-    stats = []
-try:
-    consume = consumeChild[0].parent.parent.find("div").text.replace('\n',"")
-except:
-    consume = "undefined"
-print(consume)
-print(stats)
-myJson = {
-        'id': '0',
-        'name': 'Guardian Angel',
-        'alternativeName': [],
-        'passive': passive,
-        'stats': stats,
-        'consume': consume,
-        'file': 'Guardian_Angel_item_HD.webp'
-    }
-with open('items.json', 'w', encoding='utf8') as json_file:
-    json.dump(myJson, json_file, ensure_ascii=False)
+import os
+data = {"item": [
+
+]}
+items = os.listdir('./Old items')
+for i, scuffedName in enumerate(items):
+    item = scuffedName[:scuffedName.find("_item")]
+    url = f"https://leagueoflegends.fandom.com/wiki/{item}"
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, 'html.parser')
+    passiveChild = doc.find_all(text="Passive")
+    statsChild = doc.find_all(text="Stats")
+    consumeChild = doc.find_all(text="Consume")
+    activeChild = doc.find_all(text="Active")
+    try:
+        passive = passiveChild[0].parent.parent.div.div.get_text()
+    except:
+        passive = "undefined"
+    try:
+        stats = statsChild[0].parent.parent.find_all("div",{"data-source":True})
+        for j, stat in enumerate(stats):
+            stats[j] = stat.text.replace('\n',"")
+    except:
+        stats = []
+    try:
+        consume = consumeChild[0].parent.parent.find("div").text.replace('\n',"")
+    except:
+        consume = "undefined"
+    try:
+        active = activeChild[0].parent.parent.find("div").text.replace('\n',"")
+    except:
+        active = "undefined"
+    #print(passive)
+    #print(stats)
+    #print(consume)
+    fullItem = {
+            'id': i,
+            'name': item.replace('_',' ').replace('%27','\'').replace('%28','(').replace('%29',')'),
+            'alternativeName': [],
+            'active': active,
+            'passive': passive,
+            'stats': stats,
+            'consume': consume,
+            'file': scuffedName
+        }
+    data['item'].append(fullItem)
+    if passive=="undefined" and len(stats)==0 and consume=="undefined":
+        print(fullItem)
+with open('items.json', 'w', encoding='utf8') as jsonFile:
+    json.dump(data, jsonFile, ensure_ascii=False, indent=2)
